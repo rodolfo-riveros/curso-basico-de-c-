0d6c6bc6792d023cfeccd28dc53067e106c048eb
rodolfo-riveros <76791753+rodolfo-riveros@users.noreply.github.com>
2021-02-22 16:11:36 -0500

    Add files via upload

---
+++ c++/primera fase/3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//Declaración de variables que se usan en programación 
+	
+	int numero=1;              //numero entero del 1 al infinito
+	char letra='l';            //letra, puedes escribir una letra o una palabra
+	string palabra="perro";    //una palabra o varias palabras 
+	bool verdadero=true;     //termino booleano verdadero
+	bool falso=false;        //termino booleano falso
+	double x = 3.2;            //64 bits(se usa para numeros decimales)
+    float y = 4.8;             //32 bits(se usa para numeros decimales)
+    
+    /*estructura de la impresión
+    cout<<"texto"<<variable<<enld;*/
+    cout<<"entero: "<<numero<<endl;
+    cout<<"letra: "<<letra<<endl;
+    cout<<"palabra: "<<palabra<<endl;
+    cout<<"verdadero(booleano): "<<verdadero<<endl;
+    cout<<"falso(booleano): "<<falso<<endl;
+    cout<<"decimal(64 bits): "<<x<<endl;
+    cout<<"decimal(32 bits): "<<y<<endl;
+    
+	return 0;
+}
